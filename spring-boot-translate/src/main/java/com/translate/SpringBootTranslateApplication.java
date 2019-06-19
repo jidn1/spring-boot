4573fa1aee359f2d14ec34dd626c6597b5eb349c
正经吉 <dening1644@163.com>
2019-06-19 14:40:15 +0800

    :wrench: 修改配置文件

--- spring-boot-translate/src/main/java/com/translate/SpringBootTranslateApplication.java
+++ spring-boot-translate/src/main/java/com/translate/SpringBootTranslateApplication.java
@@ -1,13 +1,21 @@
 package com.translate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.translate.*")
 @SpringBootApplication
 public class SpringBootTranslateApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(SpringBootTranslateApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTranslateApplication.class, args);
+
+        logger.info("translate启动成功---> http://localhost:8002/index");
     }
 
 }
