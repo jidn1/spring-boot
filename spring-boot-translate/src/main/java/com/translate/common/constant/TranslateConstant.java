7c357b37ed6dae7c06da737c93ceca1df37ca726
正经吉 <dening1644@163.com>
2019-08-12 20:18:12 +0800

    增加谷歌翻译api

---
+++ spring-boot-translate/src/main/java/com/translate/common/constant/TranslateConstant.java
@@ -0,0 +1,12 @@
+package com.translate.common.constant;
+
+/**
+ * @Copyright © 北京互融时代软件有限公司
+ * @Author: Jidn
+ * @Date: 2019/8/12 16:25
+ * @Description:
+ */
+public class TranslateConstant {
+
+    public static String UTF8 = "utf-8";
+}
